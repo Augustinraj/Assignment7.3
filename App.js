9b5004d162b59cd609971d7f42d5eee8faaaf5ee
Augustin <Augustinraj@users.noreply.github.com>
2016-09-30 00:11:11 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+//declaring a variable named count//
+var count = 0;
+//function add one//
+function addOne(){
+    //this is used to increment the count each time we click on button//
+  count++;
+    //Displays the incremented values in the console//
+  console.log(count);
+}
